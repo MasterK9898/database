ebb692a3c69a12674eef11ece4a3f20be5512e4e
DuGuaiK <qiyang@live.unc.edu>
2023-09-07 15:39:43 -0500

    fix: buffer test

--- A1/Main/BufferTest/source/BufferQUnit.cc
+++ A1/Main/BufferTest/source/BufferQUnit.cc
@@ -1,4 +1,3 @@
-
 #ifndef CATALOG_UNIT_H
 #define CATALOG_UNIT_H
 
@@ -43,7 +42,8 @@ void writeSymbols(char *bytes, size_t len, int i)
 int main()
 {
 
-	// QUnit::UnitTest qunit(cerr, QUnit::verbose);
+	QUnit::UnitTest qunit(cerr, QUnit::verbose);
+
 	// UNIT TEST 1: A BIG ONE!!
 	{
 
@@ -52,117 +52,108 @@ int main()
 		MyDB_TablePtr table1 = make_shared<MyDB_Table>("tempTable", "foobar");
 
 		// allocate a pinned page
-		std::cout << "allocating pinned page\n";
+		cout << "allocating pinned page\n";
 		MyDB_PageHandle pinnedPage = myMgr.getPinnedPage(table1, 0);
-		void *buffer = pinnedPage->getBytes();
-		char *bytes = (char *)buffer;
+		char *bytes = (char *)pinnedPage->getBytes();
+		writeNums(bytes, 64, 0);
+		pinnedPage->wroteBytes();
 
-		// std::cout << "void address " << buffer << " value?? " << bytes[63] << std::endl;
-		// writeNums(bytes, 64, 0);
-		// pinnedPage->wroteBytes();
+		// create a bunch of pinned pages and remember them
+		vector<MyDB_PageHandle> myHandles;
+		for (int i = 1; i < 10; i++)
+		{
+			cout << "allocating pinned page\n";
+			MyDB_PageHandle temp = myMgr.getPinnedPage(table1, i);
+			char *bytes = (char *)temp->getBytes();
+			writeNums(bytes, 64, i);
+			temp->wroteBytes();
+			myHandles.push_back(temp);
+		}
 
-		std::cout << "???????" << std::endl;
+		// now forget the pages we created
+		vector<MyDB_PageHandle> temp;
+		myHandles = temp;
 
-		// // create a bunch of pinned pages and remember them
-		// vector<MyDB_PageHandle> myHandles;
-		// for (int i = 1; i < 10; i++)
-		// {
-		// 	std::cout << i << " "
-		// 						<< "allocating pinned page\n";
-		// 	MyDB_PageHandle temp = myMgr.getPinnedPage(table1, i);
-		// 	char *bytes = (char *)temp->getBytes();
-		// 	writeNums(bytes, 64, i);
-		// 	std::cout << "wrote success\n";
-		// 	temp->wroteBytes();
-		// 	myHandles.push_back(temp);
-		// }
-
-		// std::cout << "forgetting\n";
-		// // now forget the pages we created
-		// vector<MyDB_PageHandle> temp;
-		// myHandles = temp;
-
-		// std::cout << "finished\n";
 		// now remember 8 more pages
-		// 	for (int i = 0; i < 8; i++)
-		// 	{
-		// 		cout << "allocating pinned page\n";
-		// 		MyDB_PageHandle temp = myMgr.getPinnedPage(table1, i);
-		// 		char *bytes = (char *)temp->getBytes();
+		for (int i = 0; i < 8; i++)
+		{
+			cout << "allocating pinned page\n";
+			MyDB_PageHandle temp = myMgr.getPinnedPage(table1, i);
+			char *bytes = (char *)temp->getBytes();
 
-		// 		// write numbers at the 0th position
-		// 		if (i == 0)
-		// 			writeNums(bytes, 64, i);
-		// 		else
-		// 			writeSymbols(bytes, 64, i);
-		// 		temp->wroteBytes();
-		// 		myHandles.push_back(temp);
-		// 	}
+			// write numbers at the 0th position
+			if (i == 0)
+				writeNums(bytes, 64, i);
+			else
+				writeSymbols(bytes, 64, i);
+			temp->wroteBytes();
+			myHandles.push_back(temp);
+		}
 
-		// 	// now correctly write nums at the 0th position
-		// 	cout << "allocating unpinned page \n";
-		// 	MyDB_PageHandle anotherDude = myMgr.getPage(table1, 0);
-		// 	bytes = (char *)anotherDude->getBytes();
-		// 	writeSymbols(bytes, 64, 0);
-		// 	anotherDude->wroteBytes();
+		// now correctly write nums at the 0th position
+		cout << "allocating unpinned page\n";
+		MyDB_PageHandle anotherDude = myMgr.getPage(table1, 0);
+		bytes = (char *)anotherDude->getBytes();
+		writeSymbols(bytes, 64, 0);
+		anotherDude->wroteBytes();
 
-		// 	// now do 90 more pages, for which we forget the handle immediately
-		// 	for (int i = 10; i < 100; i++)
-		// 	{
-		// 		cout << "allocating unpinned page \n";
-		// 		MyDB_PageHandle temp = myMgr.getPage(table1, i);
-		// 		char *bytes = (char *)temp->getBytes();
-		// 		writeNums(bytes, 64, i);
-		// 		temp->wroteBytes();
-		// 	}
+		// now do 90 more pages, for which we forget the handle immediately
+		for (int i = 10; i < 100; i++)
+		{
+			cout << "allocating unpinned page\n";
+			MyDB_PageHandle temp = myMgr.getPage(table1, i);
+			char *bytes = (char *)temp->getBytes();
+			writeNums(bytes, 64, i);
+			temp->wroteBytes();
+		}
 
-		// 	// now forget all of the pinned pages we were remembering
-		// 	vector<MyDB_PageHandle> temp2;
-		// 	myHandles = temp2;
+		// now forget all of the pinned pages we were remembering
+		vector<MyDB_PageHandle> temp2;
+		myHandles = temp2;
 
-		// 	// now get a pair of pages and write them
-		// 	for (int i = 0; i < 100; i++)
-		// 	{
-		// 		cout << "allocating pinned page\n";
-		// 		MyDB_PageHandle oneHandle = myMgr.getPinnedPage();
-		// 		char *bytes = (char *)oneHandle->getBytes();
-		// 		writeNums(bytes, 64, i);
-		// 		oneHandle->wroteBytes();
-		// 		cout << "allocating pinned page\n";
-		// 		MyDB_PageHandle twoHandle = myMgr.getPinnedPage();
-		// 		writeNums(bytes, 64, i);
-		// 		twoHandle->wroteBytes();
-		// 	}
+		// now get a pair of pages and write them
+		for (int i = 0; i < 100; i++)
+		{
+			cout << "allocating pinned page\n";
+			MyDB_PageHandle oneHandle = myMgr.getPinnedPage();
+			char *bytes = (char *)oneHandle->getBytes();
+			writeNums(bytes, 64, i);
+			oneHandle->wroteBytes();
+			cout << "allocating pinned page\n";
+			MyDB_PageHandle twoHandle = myMgr.getPinnedPage();
+			writeNums(bytes, 64, i);
+			twoHandle->wroteBytes();
+		}
 
-		// 	// make a second table
-		// 	MyDB_TablePtr table2 = make_shared<MyDB_Table>("tempTable2", "barfoo");
-		// 	for (int i = 0; i < 100; i++)
-		// 	{
-		// 		cout << "allocating unpinned page \n";
-		// 		MyDB_PageHandle temp = myMgr.getPage(table2, i);
-		// 		char *bytes = (char *)temp->getBytes();
-		// 		writeLetters(bytes, 64, i);
-		// 		temp->wroteBytes();
-		// 	}
-		// }
+		// make a second table
+		MyDB_TablePtr table2 = make_shared<MyDB_Table>("tempTable2", "barfoo");
+		for (int i = 0; i < 100; i++)
+		{
+			cout << "allocating unpinned page\n";
+			MyDB_PageHandle temp = myMgr.getPage(table2, i);
+			char *bytes = (char *)temp->getBytes();
+			writeLetters(bytes, 64, i);
+			temp->wroteBytes();
+		}
+	}
 
-		// // UNIT TEST 2
-		// {
-		// 	MyDB_BufferManager myMgr(64, 16, "tempDSFSD");
-		// 	MyDB_TablePtr table1 = make_shared<MyDB_Table>("tempTable", "foobar");
+	// UNIT TEST 2
+	{
+		MyDB_BufferManager myMgr(64, 16, "tempDSFSD");
+		MyDB_TablePtr table1 = make_shared<MyDB_Table>("tempTable", "foobar");
 
-		// 	// look up all of the pages, and make sure they have the correct numbers
-		// 	for (int i = 0; i < 100; i++)
-		// 	{
-		// 		MyDB_PageHandle temp = myMgr.getPage(table1, i);
-		// 		char answer[64];
-		// 		if (i < 8)
-		// 			writeSymbols(answer, 64, i);
-		// 		else
-		// 			writeNums(answer, 64, i);
-		// 		char *bytes = (char *)temp->getBytes();
-		// 		QUNIT_IS_EQUAL(string(answer), string(bytes));
-		// 	}
+		// look up all of the pages, and make sure they have the correct numbers
+		for (int i = 0; i < 100; i++)
+		{
+			MyDB_PageHandle temp = myMgr.getPage(table1, i);
+			char answer[64];
+			if (i < 8)
+				writeSymbols(answer, 64, i);
+			else
+				writeNums(answer, 64, i);
+			char *bytes = (char *)temp->getBytes();
+			QUNIT_IS_EQUAL(string(answer), string(bytes));
+		}
 	}
 }
 
