c7e02f597d3508f2d55f1a59e4792bcff846553c
DuGuaiK <qiyang@live.unc.edu>
2023-09-15 07:46:50 -0500

    chore: split page.cc out

--- A2/Main/DatabaseTable/headers/MyDB_PageReaderWriter.h
+++ A2/Main/DatabaseTable/headers/MyDB_PageReaderWriter.h
@@ -5,35 +5,37 @@
 #include "MyDB_PageType.h"
 #include "MyDB_TableReaderWriter.h"
 
-class MyDB_PageReaderWriter {
+class MyDB_PageReaderWriter
+{
 
 public:
-
 	// ANY OTHER METHODS YOU WANT HERE
 
 	// empties out the contents of this page, so that it has no records in it
 	// the type of the page is set to MyDB_PageType :: RegularPage
-	void clear ();	
+	void clear();
 
 	// return an itrator over this page... each time returnVal->next () is
 	// called, the resulting record will be placed into the record pointed to
 	// by iterateIntoMe
-	MyDB_RecordIteratorPtr getIterator (MyDB_RecordPtr iterateIntoMe);
+	MyDB_RecordIteratorPtr getIterator(MyDB_RecordPtr iterateIntoMe);
 
 	// appends a record to this page... return false is the append fails because
 	// there is not enough space on the page; otherwise, return true
-	bool append (MyDB_RecordPtr appendMe);
+	bool append(MyDB_RecordPtr appendMe);
 
 	// gets the type of this page... this is just a value from an ennumeration
 	// that is stored within the page
-	MyDB_PageType getType ();
+	MyDB_PageType getType();
 
 	// sets the type of the page
-	void setType (MyDB_PageType toMe);
-	
-private:
+	void setType(MyDB_PageType toMe);
 
+private:
 	// ANYTHING ELSE YOU WANT HERE
+
+	// current page type
+	MyDB_PageType type;
 };
 
 #endif
--- A2/Main/DatabaseTable/headers/MyDB_TableReaderWriter.h
+++ A2/Main/DatabaseTable/headers/MyDB_TableReaderWriter.h
@@ -12,43 +12,42 @@
 using namespace std;
 class MyDB_PageReaderWriter;
 class MyDB_TableReaderWriter;
-typedef shared_ptr <MyDB_TableReaderWriter> MyDB_TableReaderWriterPtr;
+typedef shared_ptr<MyDB_TableReaderWriter> MyDB_TableReaderWriterPtr;
 
-class MyDB_TableReaderWriter {
+class MyDB_TableReaderWriter
+{
 
 public:
-
 	// ANYTHING ELSE YOU NEED HERE
 
 	// create a table reader/writer for the specified table, using the specified
 	// buffer manager
-	MyDB_TableReaderWriter (MyDB_TablePtr forMe, MyDB_BufferManagerPtr myBuffer);
+	MyDB_TableReaderWriter(MyDB_TablePtr forMe, MyDB_BufferManagerPtr myBuffer);
 
 	// gets an empty record from this table
-	MyDB_RecordPtr getEmptyRecord ();
+	MyDB_RecordPtr getEmptyRecord();
 
 	// append a record to the table
-	void append (MyDB_RecordPtr appendMe);
+	void append(MyDB_RecordPtr appendMe);
 
 	// return an itrator over this table... each time returnVal->next () is
 	// called, the resulting record will be placed into the record pointed to
 	// by iterateIntoMe
-	MyDB_RecordIteratorPtr getIterator (MyDB_RecordPtr iterateIntoMe);
+	MyDB_RecordIteratorPtr getIterator(MyDB_RecordPtr iterateIntoMe);
 
 	// load a text file into this table... overwrites the current contents
-	void loadFromTextFile (string fromMe);
+	void loadFromTextFile(string fromMe);
 
 	// dump the contents of this table into a text file
-	void writeIntoTextFile (string toMe);
+	void writeIntoTextFile(string toMe);
 
 	// access the i^th page in this file
-	MyDB_PageReaderWriter operator [] (size_t i);
+	MyDB_PageReaderWriter operator[](size_t i);
 
-        // access the last page in the file
-        MyDB_PageReaderWriter last ();
+	// access the last page in the file
+	MyDB_PageReaderWriter last();
 
 private:
-
 	// ANYTHING YOU NEED HERE
 };
 
