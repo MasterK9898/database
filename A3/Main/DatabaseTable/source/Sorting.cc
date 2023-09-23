e6fff8a03f934d6ad322ed29e8b554141b034186
DuGuaiK <qiyang@live.unc.edu>
2023-09-23 15:46:45 -0500

    fix: compile error

--- A3/Main/DatabaseTable/source/Sorting.cc
+++ A3/Main/DatabaseTable/source/Sorting.cc
@@ -11,6 +11,50 @@
 
 using namespace std;
 
+// a helper function to help appending even when current page is full
+// if full create a new page and then append
+void appendHelper(MyDB_PageReaderWriter *current, MyDB_RecordPtr target, vector<MyDB_PageReaderWriter> *result, MyDB_BufferManagerPtr parent)
+{
+	if (current->append(target))
+	{
+		// if we can append, then all good
+		return;
+	}
+	else
+	{
+		// if we cannot append, then we need to create a new page
+		result->push_back(*current);
+		// create a new page
+		MyDB_PageReaderWriter newPage(*parent);
+		// swap current page into new page
+		*current = newPage;
+		// append the target, this time it must be successful
+		current->append(target);
+	}
+}
+
+// leet code merge sort, because it need recursion, so it must be separated
+vector<MyDB_PageReaderWriter> mergeHelper(size_t start, size_t end, vector<MyDB_PageReaderWriter> *currentRun, MyDB_BufferManagerPtr parent, function<bool()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs)
+{
+	if (start < end)
+	{
+		return vector<MyDB_PageReaderWriter>();
+	}
+	else if (start == end)
+	{
+		vector<MyDB_PageReaderWriter> result;
+		result.push_back(currentRun->at(start));
+		return result;
+	}
+	else
+	{
+		size_t mid = (start + end) / 2;
+		auto left = mergeHelper(start, mid, currentRun, parent, comparator, lhs, rhs);
+		auto right = mergeHelper(mid + 1, end, currentRun, parent, comparator, lhs, rhs);
+		return mergeIntoList(parent, getIteratorAlt(left), getIteratorAlt(right), comparator, lhs, rhs);
+	}
+}
+
 void mergeIntoFile(MyDB_TableReaderWriter &sortIntoMe, vector<MyDB_RecordIteratorAltPtr> &mergeUs,
 									 function<bool()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs)
 {
@@ -99,28 +143,6 @@ vector<MyDB_PageReaderWriter> mergeIntoList(MyDB_BufferManagerPtr parent, MyDB_R
 	return result;
 }
 
-// a helper function to help appending even when current page is full
-// if full create a new page and then append
-void appendHelper(MyDB_PageReaderWriter *current, MyDB_RecordPtr target, vector<MyDB_PageReaderWriter> *result, MyDB_BufferManagerPtr parent)
-{
-	if (current->append(target))
-	{
-		// if we can append, then all good
-		return;
-	}
-	else
-	{
-		// if we cannot append, then we need to create a new page
-		result->push_back(*current);
-		// create a new page
-		MyDB_PageReaderWriter newPage(*parent);
-		// swap current page into new page
-		*current = newPage;
-		// append the target, this time it must be successful
-		current->append(target);
-	}
-}
-
 void sort(int runSize, MyDB_TableReaderWriter &sortMe, MyDB_TableReaderWriter &sortIntoMe,
 					function<bool()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs)
 {
@@ -149,28 +171,6 @@ void sort(int runSize, MyDB_TableReaderWriter &sortMe, MyDB_TableReaderWriter &s
 	}
 
 	mergeIntoFile(sortIntoMe, mergeUs, comparator, lhs, rhs);
-}
-
-// leet code merge sort, because it need recursion, so it must be separated
-vector<MyDB_PageReaderWriter> mergeHelper(size_t start, size_t end, vector<MyDB_PageReaderWriter> *currentRun, MyDB_BufferManagerPtr parent, function<bool()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs)
-{
-	if (start < end)
-	{
-		return vector<MyDB_PageReaderWriter>();
-	}
-	else if (start == end)
-	{
-		vector<MyDB_PageReaderWriter> result;
-		result.push_back(currentRun->at(start));
-		return result;
-	}
-	else
-	{
-		size_t mid = (start + end) / 2;
-		auto left = mergeHelper(start, mid, currentRun, parent, comparator, lhs, rhs);
-		auto right = mergeHelper(mid + 1, end, currentRun, parent, comparator, lhs, rhs);
-		return mergeIntoList(parent, getIteratorAlt(left), getIteratorAlt(right), comparator, lhs, rhs);
-	}
 }
 
 #endif
