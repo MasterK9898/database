e4f146dfd71bff17564bf52cd8952376f89c0014
DuGuaiK <qiyang@live.unc.edu>
2023-09-25 13:41:55 -0500

    feat: migrate buffer manager

--- A3/Main/BufferMgr/source/MyDB_Page.cc
+++ A3/Main/BufferMgr/source/MyDB_Page.cc
@@ -1,47 +1,48 @@
 
-
-/****************************************************
-** COPYRIGHT 2016, Chris Jermaine, Rice University **
-**                                                 **
-** The MyDB Database System, COMP 530              **
-** Note that this file contains SOLUTION CODE for  **
-** A1.  You should not be looking at this file     **
-** unless you have completed A1!                   **
-****************************************************/
-
 #ifndef PAGE_C
 #define PAGE_C
 
-#include "MyDB_BufferManager.h"
 #include "MyDB_Page.h"
+#include "MyDB_PageHandle.h"
 #include "MyDB_Table.h"
+#include "MyDB_BufferManager.h"
 
-void *MyDB_Page :: getBytes (MyDB_PagePtr me) {
-	parent.access (me);	
-	return bytes;
+using namespace std;
+
+MyDB_Page::MyDB_Page(MyDB_TablePtr table, size_t pageIndex, MyDB_BufferManager *manager, bool pinned)
+    : bytes(nullptr), doNotKill(false), dirty(false), pinned(pinned), manager(manager), table(table), pageIndex(pageIndex), ref(0) {}
+
+MyDB_Page::~MyDB_Page()
+{
+  // no need to write back at this stage
+  // because the clock has a pointer to the page
+  // and in order to destruct the clock will need to lose the page
+  // which means an evict happens, and at that time write back already happened
 }
 
-void MyDB_Page :: wroteBytes () {
-	isDirty = true;
+void MyDB_Page::removeRef()
+{
+  this->ref--;
+  if (ref == 0)
+  {
+    // no longer pinned
+    this->pinned = false;
+
+    if (this->table == nullptr)
+    // anonymous page, leave them dying on the clock is enough
+    // they will be evicted eventually
+    // and because no one should access the anon page again
+    // there's no need to wrtie back in this situation
+    {
+      this->doNotKill = false;
+      this->bytes = nullptr;
+    }
+  }
 }
 
-MyDB_Page :: ~MyDB_Page () {}
-
-MyDB_Page :: MyDB_Page (MyDB_TablePtr myTableIn, size_t iin, MyDB_BufferManager &parentIn) : 
-	parent (parentIn), myTable (myTableIn), pos (iin) { 
-	bytes = nullptr;
-	isDirty = false;	
-	refCount = 0;
-	timeTick = -1;
-}
-
-void MyDB_Page :: killpage (MyDB_PagePtr me) {
-	parent.killPage (me);
-}
-
-MyDB_BufferManager &MyDB_Page :: getParent () {
-	return parent;	
+void MyDB_Page::addRef()
+{
+  this->ref++;
 }
 
 #endif
-
